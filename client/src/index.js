4d9eadb55e5e35e24116e265f1a03b516186447c
mmarat01 <mijaelms@protonmail.com>
2021-05-24 21:37:20 -0400

    change browserrouter to hashrouter

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './pages/App'
-import NavBar from './components/NavBar';
-import './index.css';
-import '@fontsource/roboto'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./pages/App";
+import NavBar from "./components/NavBar";
+import "./index.css";
+import "@fontsource/roboto";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <NavBar/>
-    <App/>
-  </BrowserRouter>,
-  document.getElementById('root')
+  <HashRouter>
+    <NavBar />
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
 );
