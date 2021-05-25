fe799455acab492194cc92ebb4ceee8365981ce5
mmarat01 <mijaelms@protonmail.com>
2021-05-24 21:51:01 -0400

    trying to fix client side routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./pages/App";
 import NavBar from "./components/NavBar";
 import "./index.css";
 import "@fontsource/roboto";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <NavBar />
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
