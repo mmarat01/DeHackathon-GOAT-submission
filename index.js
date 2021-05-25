e1f9ce4be725eff70609f4415ddbb37dcbef8543
mmarat01 <mijaelms@protonmail.com>
2021-05-24 22:26:31 -0400

    trying to fix client side routing

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+});
 app.use("/api/donors/", donorRouter);
 
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
